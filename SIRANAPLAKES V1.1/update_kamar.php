df182e925d32a7b5ecd20f364a708f69171f1b38
farhanrs13 <75967374+farhanrs13@users.noreply.github.com>
2022-05-25 22:19:38 +0700

    Add files via upload

---
+++ SIRANAPLAKES V1.1/update_kamar.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Update Kamar Kosong</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css" />
+    <link rel="stylesheet" type="text/css" href="responsive.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="icon" href="images/siranaplakes.png" type="image/png"><br><br><br>
+</head>
+<body>
+
+<header>
+<!-- header inner -->
+        <div class="header">
+            <div class="container">
+            <div class="row">
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                    <div class="full">
+                    <body>
+                        <div class="container">
+                            <span class="fa fa-bars menu-icon"></span>
+                            <div class="header-right">
+                                <a href="404.html">About us</a>
+                                <a href="index.php" class="login">Log out</a>
+                                <a href="404.html">Contact</a>
+                            </div>
+                            <div class="top-wrapper">
+                            <img src="images/siranaplakes.png"style="width: 150px;px;height:150px; float: left;" aligh="middle" alt="#" >
+                            </div>
+                        </div>
+                    </body>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </header>
+
+
+    <div class="letak">
+        <center>
+        <b>
+            <p>Rumah Sakit Umum Daerah Doktor Zainal Abidin.</p>
+            <p>Alamat : jln.Daud Beureueh No.108B, Banda Aceh.</p>
+            <p>Telpon : 065134271</p>
+        </b>
+        <div class="cari_rs">
+            <br>
+            <a href="404.html"><button class="button">Cek Detail</button></a>
+        </div> <br>
+        <section class="hasil">
+            <b>Kelas 1</b>
+            <p>Ruang 1 ARAFAH 3</p>
+            <p>Tersedia Kamar : 1</p>
+            <p>Bad Kosong : 3</p>
+            <p>Info : Diupdate 14 Menit Lalu</p>
+            <button class="third" onClick="alert('Pilihan Sukses!! SILAHKAN TEKAN DAFTAR')">Pilih</a></button>
+            <button class="third" href="hapus.php" onclick = "return confirm('Yakin Data Akan Dihapus');">HAPUS</a></button>
+            <br> <hr>
+            <b>Kelas 2</b>
+            <p>Ruang 3 AQSA 2</p>
+            <p>Tersedia Kamar : 1</p>
+            <p>Bad Kosong : 3</p>
+            <p>Info : Diupdate 23 Menit Lalu</p>
+            <button class="third" onClick="alert('Pilihan Sukses!! SILAHKAN TEKAN DAFTAR')">Pilih</a></button>
+            <button class="third" href="hapus.php" onclick = "return confirm('Yakin Data Akan Dihapus');">HAPUS</a></button>
+            <br> <hr>
+            <b>Kelas 1</b>
+            <p>Ruang 2 RAUDHOH 2</p>
+            <p>Tersedia Kamar : 2</p>
+            <p>Bad Kosong : 4</p>
+            <p>Info : Diupdate 38 Menit Lalu</p>
+            <button class="third" onClick="alert('Pilihan Sukses!! SILAHKAN TEKAN DAFTAR')" >Pilih</a></button>
+            <button class="third" href="hapus.php" onclick = "return confirm('Yakin Data Akan Dihapus');">HAPUS</a></button>
+        </section>
+        </center>
+    </div>
+</body>
+</html>
